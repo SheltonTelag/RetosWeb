7621413ca7b721e5827990973c1a58b44e3d906b
Shelton Telag <sheltonstuarttelagvalderrama.97@gmail.com>
2023-10-09 18:15:05 -0500

    segundo reto

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Challenge1 from './cha/challenge1.jsx'
+import Challenge1 from './Cha/Challenge1.jsx'
+import Challenge2 from './Cha/Challenge2.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Challenge1 />
+    <Challenge2 />
   </React.StrictMode>,
 )
