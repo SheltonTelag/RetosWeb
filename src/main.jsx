841e0e8ac20cdb7142c2852984e8fb79ca4c941c
Shelton Telag <sheltonstuarttelagvalderrama.97@gmail.com>
2023-10-09 17:54:59 -0500

    primer reto

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Challenge1 from './cha/challenge1.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Challenge1 />
   </React.StrictMode>,
 )
