fccb7d3959ff033b882304b7fd5c39ac2ee8bfb2
Shelton Telag <sheltonstuarttelagvalderrama.97@gmail.com>
2023-10-09 20:19:14 -0500

    reto 3

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import Challenge1 from './Cha/Challenge1.jsx'
 import Challenge2 from './Cha/Challenge2.jsx'
+import Challenge3 from './Cha/Challenge3.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Challenge2 />
+    <Challenge3 />
   </React.StrictMode>,
 )
